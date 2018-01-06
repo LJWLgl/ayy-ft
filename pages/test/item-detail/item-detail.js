45863f46cf08b0fc3469e759942cd4c0b2210cd5
nanxuan <zhiqiang.gan@duitang.com>
2018-01-07 00:02:23 +0800

    add

--- pages/test/item-detail/item-detail.js
+++ pages/test/item-detail/item-detail.js
@@ -1,16 +1,23 @@
 // pages/test/item-detail/item-detail.js
 Page({
   data: {
-    movies: [
-      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg' },
-      { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
-      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
-      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
+    imgalist: [
+      'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg',
+      'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg',
+      'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg',
+      'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
     ]
   },
   onLoad: function (options) {
     this.setData({
       goods_id: options.goods_id,
+    })
+  },
+  previewImage: function (e) {
+    var current = e.target.dataset.src;
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接  
+      urls: this.data.imgalist // 需要预览的图片http链接列表  
     })
   }
 })
