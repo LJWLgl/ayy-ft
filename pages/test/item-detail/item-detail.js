ebcce765d704f1e723ad5ea1190563a25613628d
nanxuan <zhiqiang.gan@duitang.com>
2018-01-10 23:57:46 +0800

    add

--- pages/test/item-detail/item-detail.js
+++ pages/test/item-detail/item-detail.js
@@ -1,6 +1,10 @@
 // pages/test/item-detail/item-detail.js
 Page({
   data: {
+    isShow: 0,
+    inputValue:"",
+    collectClass: 0,
+    likeClass: 0,
     imgalist: [
       'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg',
       'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg',
@@ -32,6 +36,42 @@ Page({
     wx.previewImage({
       current: current, // 当前显示图片的http链接  
       urls: this.data.imgalist // 需要预览的图片http链接列表  
+    })
+  },
+  sendClick: function (e) {
+    var _this = this;
+    _this.data.comments.push(
+      {
+        avatar: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg',
+        username: '司机峰',
+        content: e.detail.value,
+        date: '2017/12/31 08:23:00'
+      }
+    );
+    this.setData({
+      isShow: 0,
+      comments: _this.data.comments
+    })
+  },
+  commentClick: function (e) {
+    this.setData({
+      isShow: 1,
+      inputValue:''
+    })
+  },
+  unfocusInput: function (e) {
+    this.setData({
+      isShow: 0,
+    })
+  },
+  likeClick: function (e) {
+    this.setData({
+      likeClass: this.data.likeClass + 1
+    })
+  },
+  collectClick: function (e) {
+    this.setData({
+      collectClass: this.data.collectClass + 1
     })
   }
 })
