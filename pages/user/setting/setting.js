a7a1c4d755f22641ad4c6447650bdb2833fc937f
nanxuan <zhiqiang.gan@duitang.com>
2018-01-14 21:00:33 +0800

    add

--- pages/user/setting/setting.js
+++ pages/user/setting/setting.js
@@ -6,6 +6,19 @@ Page({
   },
 
   onLoad: function (options) {
-    
+
+  },
+  editConnect: function (e) {
+    wx.showModal({
+      title: '提示',
+      content: '这是一个模态弹窗',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
   }
 })
