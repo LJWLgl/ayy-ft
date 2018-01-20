145f5ef0c6d6969d2964f1f6930ef7fd27b94f2c
nanxuan <zhiqiang.gan@duitang.com>
2018-01-20 19:34:57 +0800

    add

--- pages/depoly/depoly.js
+++ pages/depoly/depoly.js
@@ -21,12 +21,21 @@ Page({
     imageList: [],
     categories:["衣服","鞋子","书本","电器","其他"],
     donationList: ["是", "否"],
+    location:'',
     dIndex: 1,
+    price:{
+      now: "",
+      old: "",
+      freight: ""
+    }
   },
   //事件处理函数
   onLoad: function () {
     console.log('onLoad')
     var that = this;
+
+    //定位
+    that.getCityNameOFLocation()
   },
   didPressChooesImage: function() {
     var _this = this;
@@ -38,6 +47,12 @@ Page({
     }, function (error) {
     });
   },
+  clickEditPrice: function(e) {
+    var _that = this;
+    wx.navigateTo({
+      url: 'edit-price/edit-price?' + 'now=' + _that.data.price.now + '&old=' + _that.data.price.old + '&freight=' + _that.data.price.freight,
+    })
+  },
   bindCategoryChange: function(e) {
     this.setData({
       index: e.detail.value
@@ -48,6 +63,51 @@ Page({
       title: '发布成功',
       icon: 'success',
       duration: 2000
+    })
+  },
+  getCityNameOFLocation: function () {
+
+    var that = this;
+    wx.getLocation({
+      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      success: function (res) {
+        console.log("定位成功");
+        var locationString = res.latitude + "," + res.longitude;
+        console.log(locationString);
+        wx.request({
+          url: 'http://apis.map.qq.com/ws/geocoder/v1/?l&get_poi=1',
+          data: {
+            "key": "YLFBZ-WHAWI-ZXUGH-53Q65-TOJ7E-ADBNQ",
+            "location": locationString
+          },
+          method: 'GET',
+          // header: {}, 
+          success: function (res) {
+            // success
+            console.log("请求成功" + res.data.result);
+            console.log("请求数据:" + res.data.result.address);
+            that.setData({
+              'location': res.data.result.address
+            })
+          },
+          fail: function () {
+            // fail
+            console.log("请求失败");
+          },
+          complete: function () {
+            // complete
+            console.log("请求完成");
+          }
+        })
+      },
+      fail: function () {
+        // fail
+        console.log("定位失败");
+      },
+      complete: function () {
+        // complete
+        console.log("定位完成");
+      }
     })
   }
 });
---
+++ pages/depoly/edit-price/edit-price.js
@@ -0,0 +1,44 @@
+// pages/depoly/edit-price/edit-price.js
+Page({
+
+  data: {
+    inputNowValue: 0,
+    inputOldValue: 0,
+    inputFreightValue: 0,
+  },
+  onLoad: function(option) {
+    this.setData({
+      'inputNowValue': option.now,
+      'inputOldValue': option.old,
+      'inputFreightValue': option.freight
+    })
+  },
+  onUnload: function () {
+    var pages = getCurrentPages();
+    var prePage = pages[pages.length - 2];
+    var _that = this;
+
+    prePage.setData({
+      price: {
+        now: _that.data.inputNowValue,
+        old: _that.data.inputOldValue,
+        freight: _that.data.inputFreightValue
+        }
+    }) 
+  },
+  bindNowInput: function(e) {
+    this.setData({
+      'inputNowValue': e.detail.value,
+    })
+  },
+  bindOldInput: function(e) {
+    this.setData({
+      'inputOldValue': e.detail.value,
+    })
+  },
+  bindFreightSlider: function(e) {
+    this.setData({
+      'inputFreightValue': e.detail.value,
+    })
+  }
+})
