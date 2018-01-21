da49bc81af3b5b8d68689a89e8cd74aaa2a0ed67
nanxuan <zhiqiang.gan@duitang.com>
2018-01-21 23:19:56 +0800

    add

--- app.js
+++ app.js
@@ -10,28 +10,42 @@ App({
     wx.login({
       success: function(res) {
         if (res.code) {
-          console.log("code " + res.code);
+          var jsCode = res.code;
+          wx.getUserInfo({
+            success: res => {
+              // this.globalData.userInfo = res.userInfo;
+              console.log("res.code " + jsCode)
+              wx.request({
+                url: 'http://localhost:8081/people/login/',
+                method: 'GET',
+                data: {
+                  code: jsCode,
+                  userInfo: res.userInfo
+                },
+                success: function(res) {
+                  console.log("success" + res.data)
+                }
+              });
+            }
+          });
         }
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+        wx.getUserInfo({
+          success: res => {
+            // 可以将 res 发送给后台解码出 unionId
+            this.globalData.userInfo = res.userInfo
+            console.log("----" + res.userInfo);
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            if (this.userInfoReadyCallback) {
+              this.userInfoReadyCallback(res)
             }
-          })
-        }
+          }
+        })
       }
     })
   },
